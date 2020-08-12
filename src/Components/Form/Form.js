d558fe9171f745465977937f4f0582e28b97bd18
Salendoil <ivantsovlexa3@gmail.com>
2020-08-12 15:17:38 +0300

    style for Form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import classes from './Form.module.css'
 import Input from '../../UI/Input/Input'
-import Button from '../../UI/Button/Button'
 
 class Form extends React.Component {
 	render(){
 		return(
 			<form className={classes.Form}>
 				<Input  />
-				<Button text="lel" />
 			</form>
 		)
 	}
