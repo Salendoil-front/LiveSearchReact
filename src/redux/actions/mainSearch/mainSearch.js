c7a1c95dd8b22bdf4bcf5cbcc1d8dd050fb1c7e3
Salendoil <ivantsovlexa3@gmail.com>
2020-08-13 18:00:47 +0300

    function for listening input and func search

--- src/redux/actions/mainSearch/mainSearch.js
+++ src/redux/actions/mainSearch/mainSearch.js
@@ -1,15 +1,27 @@
-import { CHANGE_INPUT } from "../actionTypes/actionTypes";
-
-export function findLi(input, peopleArr){
-	let val = input
-	let peopleArray = peopleArr
-	
-	
+export function findLi(value, peopleArr) {
+	return dispatch => {
+		let val = value.toLowerCase().trim()
+		let peopleArray = peopleArr
+		if(val !== ''){
+			peopleArray.forEach((person) => {
+				if(person.name.trim().toLowerCase().search(val) === -1 && String(person.age).trim().search(val) === -1 && String(person.money).trim().search(val) === -1){
+					person.class = false
+				} else{
+					person.class = true
+				}
+			})
+		} else{
+			peopleArray.forEach(person => {
+				person.class = true
+			})
+		}
+		dispatch(changeClass(peopleArray))
+	}
 }
 
-export function changeInput(value){
+export function changeClass (people){
 	return{
-		type: CHANGE_INPUT,
-		payload: value
+		type: 'ChangeClass',
+		payload: people
 	}
 }
